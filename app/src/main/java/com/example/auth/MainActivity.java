ada5c9e84d5e07f2c4df46d37d7252f865eeef48
msnjkmsn <94446637+MShashaIE@users.noreply.github.com>
2022-01-08 20:25:43 +0000

    added a ScorePage

--- app/src/main/java/com/example/auth/MainActivity.java
+++ app/src/main/java/com/example/auth/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -25,8 +28,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        question = findViewById(R.id.questionAttempted);
-        questionNumber = findViewById(R.id.question);
+        question = findViewById(R.id.question);
+        questionNumber = findViewById(R.id.questionAttempted);
         optionBtn1 = findViewById(R.id.option1);
         optionBtn2 = findViewById(R.id.option2);
         optionBtn3 = findViewById(R.id.option3);
@@ -39,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         optionBtn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (quizModalArrayList.get(currentPos).getAnswer().trim().toLowerCase(Locale.ROOT).equals(optionBtn1.getText().toString().trim().toLowerCase())) {
+                if (quizModalArrayList.get(currentPos).getAnswer().trim().toLowerCase().equals(optionBtn1.getText().toString().trim().toLowerCase())) {
                     currentScore++;
                 }
                 questionAttempted++;
@@ -47,12 +50,49 @@ public class MainActivity extends AppCompatActivity {
                 setDataToViews(currentPos);
             }
         });
-
+        optionBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (quizModalArrayList.get(currentPos).getAnswer().trim().toLowerCase().equals(optionBtn2.getText().toString().trim().toLowerCase())) {
+                    currentScore++;
+                }
+                questionAttempted++;
+                currentPos = random.nextInt(quizModalArrayList.size());
+                setDataToViews(currentPos);
+            }
+        });
+        optionBtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (quizModalArrayList.get(currentPos).getAnswer().trim().toLowerCase().equals(optionBtn3.getText().toString().trim().toLowerCase())) {
+                    currentScore++;
+                }
+                questionAttempted++;
+                currentPos = random.nextInt(quizModalArrayList.size());
+                setDataToViews(currentPos);
+            }
+        });
+        optionBtn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (quizModalArrayList.get(currentPos).getAnswer().trim().toLowerCase().equals(optionBtn4.getText().toString().trim().toLowerCase())) {
+                    currentScore++;
+                }
+                questionAttempted++;
+                currentPos = random.nextInt(quizModalArrayList.size());
+                setDataToViews(currentPos);
+            }
+        });
     }
+
+
 
     private void setDataToViews(int currentPos) {
 
         questionNumber.setText("Questions attempted : " + questionAttempted + " /10");
+        if(questionAttempted==10){
+            showSheet();
+        }
         question.setText(quizModalArrayList.get(currentPos).getQuestion());
         optionBtn1.setText(quizModalArrayList.get(currentPos).getOption1());
         optionBtn2.setText(quizModalArrayList.get(currentPos).getOption2());
@@ -62,9 +102,30 @@ public class MainActivity extends AppCompatActivity {
 
     private void getQuizQuestion(ArrayList<QuizModal> quizModalArrayList) {
 
-        quizModalArrayList.add(new QuizModal("1. Which of the following keyword is not associated with IF statement?", "ELSE", "THEN", "ELSE IF", "WHEN", "WHEN"));
-        quizModalArrayList.add(new QuizModal("2. An ELSE statement must be preceded by ___ statement in Java.", "IF", "ELSE IF", "IF or ELSE IF", "NONE", "IF or ELSE IF"));
-        quizModalArrayList.add(new QuizModal("3. An IF or ELSE IF statement accepts ___ as input before branching.", "boolean", "int", "char", "float", "boolean"));
-        quizModalArrayList.add(new QuizModal("1. An IF statement in Java is also a ___ statement.", "boolean", "conditional", "iterative", "optional", "conditional"));
+        quizModalArrayList.add(new QuizModal("Which of the following keyword is not associated with IF statement?", "ELSE", "THEN", "ELSE IF", "WHEN", "WHEN"));
+        quizModalArrayList.add(new QuizModal("An ELSE statement must be preceded by ___ statement in Java.", "IF", "ELSE IF", "IF or ELSE IF", "NONE", "IF or ELSE IF"));
+        quizModalArrayList.add(new QuizModal("An IF or ELSE IF statement accepts ___ as input before branching.", "boolean", "int", "char", "float", "boolean"));
+        quizModalArrayList.add(new QuizModal("An IF statement in Java is also a ___ statement.", "boolean", "conditional", "iterative", "optional", "conditional"));
+    }
+
+    private void showSheet(){
+        BottomSheetDialog bottomSheetDialog  = new BottomSheetDialog(MainActivity.this);
+        View bottomSheetView  = getLayoutInflater().from(getApplicationContext()).inflate(R.layout.score,(LinearLayout)findViewById(R.id.LLScore));
+        TextView scoreTV  =  bottomSheetView.findViewById(R.id.score);
+        Button restartQuiz = bottomSheetView.findViewById(R.id.restartQuiz);
+        scoreTV.setText("Your Score is "+currentScore+ "/10");
+        restartQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentPos = random.nextInt(quizModalArrayList.size());
+                setDataToViews(currentPos);
+                questionAttempted = 1 ;
+                currentScore = 0;
+                bottomSheetDialog.dismiss();
+            }
+        });
+        bottomSheetDialog.setCancelable(false);
+        bottomSheetDialog.setContentView(bottomSheetView);
+        bottomSheetDialog.show();
     }
 }
